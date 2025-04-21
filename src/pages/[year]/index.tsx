134d6fc6c4fc4e53c97715902c4dbfaa04749a57
eben619 <ebenyawboateng72@gmail.com>
2025-04-21 13:02:15 +0000

    remove unused import for EthAccra2024 component

--- src/pages/[year]/index.tsx
+++ src/pages/[year]/index.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import EthAccra2023 from "@/components/2023/main";
-import EthAccra2024 from "@/components/2024/main";
 import { useParams, useRouter } from "next/navigation";
 
 type ValidYears = "2023" | "2024";
@@ -12,7 +11,6 @@ const EthAccraTemplate = () => {
 
   const pageViews = {
     "2023": <EthAccra2023 />,
-    "2024": <EthAccra2024 />,
   };
 
   const returnView = () => {
