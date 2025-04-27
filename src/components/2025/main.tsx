7ead873f924f2baa7507c8c77731a2d43d712e79
eben619 <ebenyawboateng72@gmail.com>
2025-04-27 13:50:09 +0000

    fix: reorder Head component structure for improved SEO clarity

--- src/components/2025/main.tsx
+++ src/components/2025/main.tsx
@@ -23,31 +23,34 @@ const EthAccra2025 = () => {
   return (
     <div>
       <Head>
-        <title>{title}</title>
-        <link rel="canonical" href={url} />
-        
-        {/* Basic SEO */}
-        <meta name="description" content={description} />
-        
-        {/* Open Graph / Facebook */}
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content={url} />
-        <meta property="og:site_name" content={siteName} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:image:secure_url" content={imageUrl} />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="400" />
-        <meta property="og:image:height" content="300" />
-        
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={imageUrl} />
-      </Head>
+          <title>{title}</title>
+          <link rel="canonical" href={url} />
+          
+          {/* Favicon */}
+          <link rel="icon" href="/favicon.ico" />
+          
+          {/* Basic SEO */}
+          <meta name="description" content={description} />
+          
+          {/* Open Graph / Facebook */}
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:url" content={url} />
+          <meta property="og:site_name" content={siteName} />
+          <meta property="og:image" content={imageUrl} />
+          <meta property="og:image:secure_url" content={imageUrl} />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="400" />
+          <meta property="og:image:height" content="300" />
+          
+          {/* Twitter */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:title" content={title} />
+          <meta property="twitter:description" content={description} />
+          <meta property="twitter:image" content={imageUrl} />
+        </Head>
 
       <div className="">
         <Illustration />
